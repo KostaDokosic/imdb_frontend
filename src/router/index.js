fd0698aec8b5243a5e20b32c4b864872b7f750da
KostaDokosic <kosta99dt@gmail.com>
2022-05-12 14:42:19 +0200

    added router & store

---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeLayout from '../components/HomeLayout.vue';
+import AuthLayout from '../components/AuthLayout.vue';
+
+import Dashboard from '../views/Dashboard.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+
+import store from '../store';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        redirect: '/dashboard',
+        component: HomeLayout,
+        meta: {requireAuth: true},
+        children: [
+            {
+                path: '/dashboard', 
+                name: 'Dashboard',
+                component: Dashboard
+            }
+        ]
+    },
+    {
+        path: '/auth',
+        name: 'Auth',
+        redirect: '/login',
+        component: AuthLayout,
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            }
+        ]
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requireAuth && !store.state.user.token) next({name: 'Login'})
+    else if(store.state.user.token) next({name: 'Dashboard'})
+    else next();
+});
+
+export default router;
